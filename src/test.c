cbb936564e579c4578b5a88dcb58115753b37494
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-03-02 11:27:09 +0100

    added write_h5 test

--- src/test.c
+++ src/test.c
@@ -8,14 +8,69 @@ int test_read();
 int test_write();
 
 int test_h5read();
+int test_h5write();
 
 int main() {
+  test_h5write();
   test_h5read();
   test_write();
   test_read();
   return 0 ;
 }
 
+
+int test_h5write() {
+  const char* file_name = "test_write.h5";
+
+  trio_t* file = NULL;
+  trio_exit_code rc;
+
+  rc = TRIO_SUCCESS;
+
+  uint64_t num = 12;
+
+  //double charge[12] = {6., 6., 6., 6., 6., 6., 1., 1., 1., 1., 1., 1.};
+
+  double coord[36] = {
+  0.00000000 ,  1.39250319 ,  0.00000000 ,
+ -1.20594314 ,  0.69625160 ,  0.00000000 ,
+ -1.20594314 , -0.69625160 ,  0.00000000 ,
+  0.00000000 , -1.39250319 ,  0.00000000 ,
+  1.20594314 , -0.69625160 ,  0.00000000 ,
+  1.20594314 ,  0.69625160 ,  0.00000000 ,
+ -2.14171677 ,  1.23652075 ,  0.00000000 ,
+ -2.14171677 , -1.23652075 ,  0.00000000 ,
+  0.00000000 , -2.47304151 ,  0.00000000 ,
+  2.14171677 , -1.23652075 ,  0.00000000 ,
+  2.14171677 ,  1.23652075 ,  0.00000000 ,
+  0.00000000 ,  2.47304151 ,  0.00000000 ,
+  };
+
+  file = trio_create(file_name, TRIO_HDF5);
+
+  // works: try writing info in an empty file
+  rc = trio_write_nucleus_num(file,num);
+//  rc = trio_write_nucleus_charge(file,charge);
+  rc = trio_write_nucleus_coord(file,coord);
+
+  // should not work: try to rewrite the nucleus_num
+  rc = trio_write_nucleus_num(file,25);
+
+  // works: try to rewrite the nucleus_coord 
+ // coord[0] = 666.0;
+ // rc = trio_write_nucleus_coord(file,coord);
+
+
+  if (rc == TRIO_SUCCESS) {
+    printf("SUCCESS\n");
+  } else {
+    printf("FAILURE\n");
+  }
+
+  trio_close(file);
+
+  return 0;
+}
 
 int test_h5read() {
   const char* file_name = "test.h5";
@@ -29,23 +84,24 @@ int test_h5read() {
 
   file = trio_create(file_name, TRIO_HDF5);
 
-  /*rc = trio_read_nucleus_num(file,&num);
-  assert (num == 12);
+  rc = trio_read_nucleus_num(file,&num);
+  assert (num == 4);
 
+  /*
   charge = (double*) calloc(num, sizeof(double));
   rc = trio_read_nucleus_charge(file,charge);
-  assert(charge[10] == 1.);*/
-  num = 4;
+  assert(charge[] == 1.);
+  */
 
   coord = (double*) calloc(3*num, sizeof(double));
   rc = trio_read_nucleus_coord(file,coord);
   
-  for (size_t i=0; i<3*num; i++){
+  /*for (size_t i=0; i<3*num; i++){
 	  printf("%lf \n", coord[i]);
-  }
+  }*/
 
-  double x = coord[0] - 2.14171677;
-  assert( x*x < 1000.);
+  double x = coord[0] - 1.2;
+  assert( x*x < 1.e-12);
 
   if (rc == TRIO_SUCCESS) {
     printf("SUCCESS\n");
@@ -53,7 +109,9 @@ int test_h5read() {
     printf("FAILURE\n");
   }
 
+
   trio_close(file);
+  free(coord);
 
   return 0;
 }
@@ -133,6 +191,9 @@ int test_read() {
   }
 
   trio_close(file);
+
+  free(charge);
+  free(coord);
 
   return 0;
 }
