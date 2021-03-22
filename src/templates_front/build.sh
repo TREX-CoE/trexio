7f3dc0879c9ee872f37006c5992c1528be055197
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-03-22 16:15:07 +0100

    wip: write nums

--- src/templates_front/build.sh
+++ src/templates_front/build.sh
@@ -2,14 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# prefixes
 cat $DIR/prefix_front.c   > trexio.c
 cat $DIR/prefix_front.h   > trexio.h
 cat $DIR/prefix_s_front.h > trexio_s.h
+cat $DIR/prefix_fortran.f90   > trexio_f.f90
 
+# c front end
 cat $DIR/populated/pop_*.c >> trexio.c
 cat $DIR/populated/pop_*.h >> trexio.h
 
+# fortran front end
+cat $DIR/populated/pop_*.f90  >> trexio_f.f90
+
+# suffixes
 cat $DIR/suffix_s_front.h >> trexio_s.h
 cat $DIR/suffix_front.h   >> trexio.h
-
+cat $DIR/suffix_fortran.f90 >> trexio_f.f90
 
