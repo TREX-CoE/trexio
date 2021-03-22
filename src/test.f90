ac44fc29a64b74d2b953103ce7b9a637663e75bc
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-03-22 16:40:45 +0100

    add test, works for h5 but failt for txt

---
+++ src/test.f90
@@ -0,0 +1,24 @@
+program test_trexio
+
+  use trexio
+
+  implicit none
+
+  type(c_ptr) :: file1
+
+  integer :: rc 
+  integer (c_int64_t) :: num 
+
+  rc = 0
+  num = 10
+
+  file1 = trexio_open('test_fort' // c_null_char, 'w', 1)
+!  file1 = trexio_open('test_fort.h5' // c_null_char, 'w', 0)
+
+  rc = trexio_write_nucleus_num(file1, num)
+  if (rc == 0) write(*,*) 'SUCCESS 1'
+
+  rc = trexio_close(file1)
+  if (rc == 0) write(*,*) 'SUCCESS 2' 
+
+end program test_trexio
