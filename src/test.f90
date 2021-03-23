d680120efea6fa67791cc4028530b40d4eaba969
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-03-23 14:57:11 +0100

    working write_num [fortran]

--- src/test.f90
+++ src/test.f90
@@ -12,7 +12,7 @@ program test_trexio
   rc = 0
   num = 10
 
-  file1 = trexio_open('test_fort' // c_null_char, 'w', 1)
+  file1 = trexio_open('trexio_test' // c_null_char, 'w', 1)
 !  file1 = trexio_open('test_fort.h5' // c_null_char, 'w', 0)
 
   rc = trexio_write_nucleus_num(file1, num)
