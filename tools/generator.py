da93863a3398deb7ce26eaa08dcf9560d49fe8ad
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-05-26 16:28:35 +0200

    comment main code blocks

--- tools/generator.py
+++ tools/generator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from generator_tools import *
 
+# --------------------- GET CONFIGURATION FROM THE TREX.JSON ---------------- #
 config_file = 'trex.json'
 trex_config = read_json(config_file)
+# --------------------------------------------------------------------------- #
 
+# -------------------------------- [WIP] ------------------------------------ #
 print('Metadata I/O currently not supported')
 # TODO, for now remove metadata-related stuff
 del trex_config['metadata']
@@ -11,26 +14,31 @@ del trex_config['metadata']
 del trex_config['rdm']
 # TODO, for now remove char-related stuff
 print('Strings I/O currently not supported')
+# --------------------------------------------------------------------------- #
 
-# ------------------ REDO -------------------
+# -------------------- GET ATTRIBUTES FROM THE CONFIGURATION ---------------- #
 group_dict = get_group_dict(trex_config)
 detailed_num = get_detailed_num_dict(trex_config)
-
+# helper dictionaries contain group, num or dset names as keys
 datasets = get_dset_dict(trex_config)
 detailed_dset_nostr, detailed_dset_str = split_dset_dict_detailed(datasets)
+# --------------------------------------------------------------------------- #
 
-# ------------------ REDO -------------------
-
+# -------------------- GET TEMPLATED FILES TO BE POPULATED ------------------ #
 source = ['front', 'text', 'hdf5']
-# build helper dictionaries with paths and source files per source directory 
+# build helper dictionaries with paths per source directory 
 template_paths = get_template_paths(source)
+# build helper dictionaries with source files per source directory 
 source_files = get_source_files(template_paths)
 # build helper dictionaries with templated files
 files_todo = get_files_todo(source_files)
+# --------------------------------------------------------------------------- #
+
+# ----------------------- POPULATE TEMPLATED FILES -------------------------- #
 
 # populate files with iterative scheme, i.e. for unique functions
 for fname in files_todo['auxiliary']:
-    iterative_populate_file(fname, template_paths, detailed_dset_nostr, detailed_num, group_dict)
+    iterative_populate_file(fname, template_paths, group_dict, detailed_dset_nostr, detailed_num)
 
 # populate has/read/write_num functions with recursive scheme
 for fname in files_todo['num']:
@@ -43,3 +51,5 @@ for fname in files_todo['dset']:
 # populate group-related functions with mixed (iterative+recursive) scheme [text backend]
 for fname in files_todo['group']:
     special_populate_text_group(fname, template_paths, group_dict, detailed_dset_nostr, detailed_num)
+
+# --------------------------------------------------------------------------- #
