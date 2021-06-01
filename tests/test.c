69ffb6c3b58c1d48b7ab2451cf6388206d95bde0
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-06-01 15:03:37 +0200

    Tests fail

--- tests/test.c
+++ tests/test.c
@@ -5,28 +5,29 @@
 
 int test_read();
 int test_write();
-int test_h5read();
-int test_h5write();
 
 int main() {
 
 /*============== Main test launcher ================*/
 
-  test_h5write();
-  test_h5read();
+  int rc;
+  rc = system("rm -rf test_write.h5");
+  assert (rc == 0);
+  test_write("test_write.h5", TREXIO_HDF5);
+  test_read ("test_write.h5", TREXIO_HDF5);
 
-  test_write();
-  test_read();
+  rc = system("rm -rf test_write.dir");
+  assert (rc == 0);
+  test_write("test_write.dir", TREXIO_TEXT);
+  test_read ("test_write.dir", TREXIO_TEXT);
 
-  printf("Test error message: %s\n", trexio_string_of_error(TREXIO_INVALID_ARG_2));
-  return 0 ;
+  return 0;
 }
 
-int test_h5write() {
+int test_write(const char* file_name, const back_end_t backend) {
 
-/*======== Test write using HDF5 backend ===========*/
+/*======== Test write ===========*/
 
-  const char* file_name = "test_write.h5";
   trexio_t* file = NULL;
   trexio_exit_code rc;
 
@@ -52,7 +53,7 @@ int test_h5write() {
 /*================= START OF TEST ==================*/
 
   // open file in 'write' mode
-  file = trexio_open(file_name, 'w', TREXIO_HDF5);
+  file = trexio_open(file_name, 'w', backend);
   assert (file != NULL);
 
   // check that certain data does not exist in the file
@@ -77,6 +78,7 @@ int test_h5write() {
 
   // should not work: try to overwrite the nucleus_num
   rc = trexio_write_nucleus_num(file,25);
+  printf("Test error message: %s\n", trexio_string_of_error(rc));
   assert (rc == TREXIO_NUM_ALREADY_EXISTS);
 
   // close current session
@@ -86,7 +88,7 @@ int test_h5write() {
   // open file again in 'append' mode
   file = trexio_open(file_name, 'a', TREXIO_HDF5);
   assert (file != NULL);
-  
+
   // read the nucleus_num from existing file
   rc = trexio_read_nucleus_num(file,&num);
   assert (rc == TREXIO_SUCCESS);
@@ -106,11 +108,10 @@ int test_h5write() {
   return 0;
 }
 
-int test_h5read() {
+int test_read(const char* file_name, const back_end_t backend) {
 
-/*========= Test read using HDF5 backend ===========*/
+/*========= Test read ===========*/
 
-  const char* file_name = "test_write.h5";
   trexio_t* file = NULL;
   trexio_exit_code rc;
 
@@ -119,8 +120,8 @@ int test_h5read() {
 
 /*================= START OF TEST ==================*/
 
-  // open existing file on 'read' mode [created by test_h5write()]
-  file = trexio_open(file_name, 'r', TREXIO_HDF5);
+  // open existing file on 'read' mode [created by test_write]
+  file = trexio_open(file_name, 'r', backend);
   assert (file != NULL);
 
   // read nucleus_num
@@ -141,16 +142,16 @@ int test_h5read() {
   assert (rc == TREXIO_SUCCESS);
 
   // read non-existing file, should fail and return NULL
-  const char* file_name2 = "test_nonexisting.h5";
+  const char* file_name2 = "test_nonexisting";
   trexio_t* file2 = NULL;
 
-  file2 = trexio_open(file_name2, 'r', TREXIO_HDF5);
+  file2 = trexio_open(file_name2, 'r', backend);
   assert (file2 == NULL);
 
   // append non-existing file, should fail and return NULL
   trexio_t* file3 = NULL;
 
-  file3 = trexio_open(file_name2, 'a', TREXIO_HDF5);
+  file3 = trexio_open(file_name2, 'a', backend);
   assert (file3 == NULL);
 
 /*================= END OF TEST =====================*/
@@ -159,105 +160,4 @@ int test_h5read() {
   return 0;
 }
 
-
-int test_write() {
-
-/*========= Test write using TEXT backend ===========*/
-
-  const char* file_name = "trexio_test";
-  trexio_t* file = NULL;
-  trexio_exit_code rc;
-
-  // parameters to be written
-  int num = 12;
-  float charge[12] = {6., 6., 6., 6., 6., 6., 1., 1., 1., 1., 1., 1.};
-  double coord[36] = {
-  0.00000000 ,  1.39250319 ,  0.00000000 ,
- -1.20594314 ,  0.69625160 ,  0.00000000 ,
- -1.20594314 , -0.69625160 ,  0.00000000 ,
-  0.00000000 , -1.39250319 ,  0.00000000 ,
-  1.20594314 , -0.69625160 ,  0.00000000 ,
-  1.20594314 ,  0.69625160 ,  0.00000000 ,
- -2.14171677 ,  1.23652075 ,  0.00000000 ,
- -2.14171677 , -1.23652075 ,  0.00000000 ,
-  0.00000000 , -2.47304151 ,  0.00000000 ,
-  2.14171677 , -1.23652075 ,  0.00000000 ,
-  2.14171677 ,  1.23652075 ,  0.00000000 ,
-  0.00000000 ,  2.47304151 ,  0.00000000 ,
-  };
-
-/*================= START OF TEST ==================*/
-
-  file = trexio_open(file_name, 'w', TREXIO_TEXT);
-  assert (file != NULL);
-
-  rc = trexio_has_nucleus_num(file);
-  assert (rc == TREXIO_HAS_NOT);
-  rc = trexio_has_nucleus_coord(file);
-  assert (rc == TREXIO_HAS_NOT);
-
-  rc = trexio_write_nucleus_num(file,num);
-  assert (rc == TREXIO_SUCCESS);
-
-  rc = trexio_write_nucleus_charge_32(file,charge);
-  assert (rc == TREXIO_SUCCESS);
-
-  rc = trexio_write_nucleus_coord(file,coord);
-  assert (rc == TREXIO_SUCCESS);
-
-  rc = trexio_has_nucleus_num(file);
-  assert (rc == TREXIO_SUCCESS);
-  rc = trexio_has_nucleus_coord(file);
-  assert (rc == TREXIO_SUCCESS);
-
-  rc = trexio_close(file);
-  assert (rc == TREXIO_SUCCESS);
-
-/*================= END OF TEST =====================*/
-
-  return 0;
-}
-
-int test_read() {
-
-/*========= Test read using TEXT backend ===========*/
-	
-  const char* file_name = "trexio_test";
-  trexio_t* file = NULL;
-  trexio_exit_code rc;
-
-  int num;
-  float* charge;
-  double* coord;
-
-/*================= START OF TEST ==================*/
-
-  file = trexio_open(file_name, 'r', TREXIO_TEXT);
-  assert (file != NULL);
-
-  rc = trexio_read_nucleus_num(file,&num);
-  assert (rc == TREXIO_SUCCESS);
-  assert (num == 12);
-
-  charge = (float*) calloc(num, sizeof(float));
-  rc = trexio_read_nucleus_charge_32(file,charge);
-  assert (rc == TREXIO_SUCCESS);
-  assert(charge[10] == 1.);
-
-  coord = (double*) calloc(3*num, sizeof(double));
-  rc = trexio_read_nucleus_coord(file,coord);
-  assert (rc == TREXIO_SUCCESS);
-
-  double x = coord[30] - 2.14171677;
-  assert(x*x < 1.e-14);
-
-  rc = trexio_close(file);
-  assert (rc == TREXIO_SUCCESS);
-
-/*================= END OF TEST =====================*/
-
-  free(charge);
-  free(coord);
-  return 0;
-}
 
