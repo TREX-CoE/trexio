4287d76de271b81216cab4e875b4b0b2820e1a09
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-06-14 11:54:10 +0200

    introduce calls to trexio_assert and trexio_strarray2str subroutines

--- tests/test_f.f90
+++ tests/test_f.f90
@@ -14,7 +14,7 @@ program test_trexio
   call test_write('test_write_f.h5', TREXIO_HDF5)
   print *, 'call test_read(''test_write_f.h5'', TREXIO_HDF5)'
   call test_read('test_write_f.h5', TREXIO_HDF5)
-!  call system('rm -rf test_write_f.h5')
+  call system('rm -rf test_write_f.h5')
 
 end program test_trexio
 
@@ -34,10 +34,7 @@ subroutine test_write(file_name, back_end)
 
   integer :: rc = 1
 
-  integer :: i
   integer :: num
-  character*(128) :: str
-
   double precision :: charge(12)
   double precision :: coord(3,12)
 
@@ -64,115 +61,49 @@ subroutine test_write(file_name, back_end)
 
   label = [character(len=8) :: 'C', 'Na','C', 'C 66', 'C','C', 'H 99', 'Ru', 'H', 'H',  'H', 'H' ]
 
-  label_str = ''
-  do i = 1,num
-    label_str = label_str // trim(label(i)) // TREXIO_DELIM
-  enddo
-  label_str = label_str // c_null_char
+  call trexio_strarray2str(label, num, 4, label_str)
 
-  sym_str = 'B3U with some juice' // c_null_char
+  sym_str = 'B3U with some comments' // c_null_char
+
 ! ================= START OF TEST ===================== !
 
   trex_file = trexio_open(file_name, 'w', back_end)
 
   rc = trexio_has_nucleus_num(trex_file)
-  if (rc == TREXIO_HAS_NOT) then
-    write(*,*) 'SUCCESS HAS NOT 1'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_HAS_NOT, 'SUCCESS HAS NOT 1')
 
   rc = trexio_has_nucleus_charge(trex_file)
-  if (rc == TREXIO_HAS_NOT) then
-    write(*,*) 'SUCCESS HAS NOT 2'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
-
+  call trexio_assert(rc, TREXIO_HAS_NOT, 'SUCCESS HAS NOT 2')
 
   rc = trexio_write_nucleus_num(trex_file, num)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS WRITE NUM'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS WRITE NUM')
 
   rc = trexio_write_nucleus_charge(trex_file, charge)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS WRITE CHARGE'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS WRITE CHARGE')
 
   rc = trexio_write_nucleus_coord(trex_file, coord)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS WRITE COORD'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS WRITE COORD')
 
-  if (back_end == TREXIO_HDF5) rc = trexio_write_nucleus_label(trex_file, label_str, 8)
+  rc = trexio_write_nucleus_label(trex_file, label_str, 5)
   deallocate(label_str)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS WRITE LABEL'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS WRITE LABEL')
 
-  if (back_end == TREXIO_HDF5) rc = trexio_write_nucleus_point_group(trex_file, sym_str, 32)
+  rc = trexio_write_nucleus_point_group(trex_file, sym_str, 32)
   deallocate(sym_str)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS WRITE POINT GROUP'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS WRITE POINT GROUP')
 
   rc = trexio_has_nucleus_num(trex_file)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS HAS 1'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS HAS 1')
 
   rc = trexio_has_nucleus_coord(trex_file)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS HAS 2'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS HAS 2')
 
   rc = trexio_close(trex_file)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS CLOSE'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
+  call trexio_assert(rc, TREXIO_SUCCESS, 'SUCCESS CLOSE')
 
 ! ================= END OF TEST ===================== !
 
 end subroutine test_write
-
-
 
 
 subroutine test_read(file_name, back_end)
@@ -208,42 +139,39 @@ subroutine test_read(file_name, back_end)
 
   trex_file = trexio_open(file_name, 'r', back_end)
 
-  rc = trexio_read_nucleus_num(trex_file, num_read)
 
-  if (rc == TREXIO_SUCCESS .and. num_read == num) then
+  rc = trexio_read_nucleus_num(trex_file, num_read)
+  call trexio_assert(rc, TREXIO_SUCCESS)
+  if (num_read == num) then
     write(*,*) 'SUCCESS READ NUM'
   else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
+    print *, 'FAILURE NUM CHECK'
+    call exit(-1)
   endif
 
 
   rc = trexio_read_nucleus_charge(trex_file, charge)
-
-  if (rc == TREXIO_SUCCESS .and. (dabs(charge(11) - 1.d0) < 1.0D-8) ) then
+  call trexio_assert(rc, TREXIO_SUCCESS)
+  if (dabs(charge(11) - 1.d0) < 1.0D-8) then
     write(*,*) 'SUCCESS READ CHARGE'
   else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
+    print *, 'FAILURE CHARGE CHECK'
     call exit(-1)
   endif
+
 
   rc = trexio_read_nucleus_coord(trex_file, coord)
-
-  if (rc == TREXIO_SUCCESS .and. (dabs(coord(2,1) - 1.39250319d0) < 1.0D-8) ) then
+  call trexio_assert(rc, TREXIO_SUCCESS)
+  if (dabs(coord(2,1) - 1.39250319d0) < 1.0D-8) then
     write(*,*) 'SUCCESS READ COORD'
   else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
+    print *, 'FAILURE COORD CHECK'
     call exit(-1)
   endif
 
-  if (back_end == TREXIO_HDF5) then 
   
   rc = trexio_read_nucleus_label(trex_file, label_str, 2)
-
-  ! write(*,*) label_str
+  call trexio_assert(rc, TREXIO_SUCCESS)
   ! --------------------------------------------------
   ! dummy parser of big string with TREXIO_DELIM delimeters
   ! --------------------------------------------------
@@ -265,38 +193,26 @@ subroutine test_read(file_name, back_end)
     write(*,*) label(i)
     offset=ind
   enddo
-
   ! --------------------------------------------------
-
-  if (rc == TREXIO_SUCCESS .and. (trim(label(2)) == 'Na') ) then
+  if (trim(label(2)) == 'Na') then
     write(*,*) 'SUCCESS READ LABEL'
   else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
+    print *, 'FAILURE LABEL CHECK'
     call exit(-1)
   endif
+
 
   rc = trexio_read_nucleus_point_group(trex_file, sym_str, 32)
-
-  if (rc == TREXIO_SUCCESS .and. (sym_str(1:3) == 'B3U') ) then
+  call trexio_assert(rc, TREXIO_SUCCESS)
+  if (sym_str(1:3) == 'B3U') then
     write(*,*) 'SUCCESS READ POINT GROUP'
   else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
+    print *, 'FAILURE POINT GROUP CHECK'
     call exit(-1)
-  endif
-
   endif
 
   rc = trexio_close(trex_file)
-  if (rc == TREXIO_SUCCESS) then
-    write(*,*) 'SUCCESS CLOSE'
-  else
-    call trexio_string_of_error(TREXIO_READONLY,str)
-    print *, trim(str)
-    call exit(1)
-  endif
-
+  call trexio_assert(rc, TREXIO_SUCCESS)
 
 ! ================= END OF TEST ===================== !
 
