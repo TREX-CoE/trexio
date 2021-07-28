f3dbfc7be226976d092b89eb247ad7c1e35c80b7
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-07-28 10:33:33 +0200

    added License and install_requires in setup.py + cleaning

--- src/setup.py
+++ src/setup.py
@@ -8,20 +8,22 @@ from distutils.core import setup, Extension
 
 
 pytrexio_module = Extension('_pytrexio',
-                           sources=['trexio.c', 'trexio_hdf5.c', 'trexio_text.c', 'pytrexio_wrap.c'],
-                           include_dirs=['/usr/include/hdf5/serial'],
-                           #runtime_library_dirs=['/usr/lib/x86_64-linux-gnu/hdf5/serial'],
-                           libraries=['hdf5', 'hdf5_hl'],
-                           extra_compile_args=['-Wno-discarded-qualifiers'],
-                           extra_link_args=['-L/usr/lib/x86_64-linux-gnu/hdf5/serial']
-                           )
+                            sources = ['trexio.c', 'trexio_hdf5.c', 'trexio_text.c', 'pytrexio_wrap.c'],
+                            include_dirs = ['/usr/include/hdf5/serial'],
+                            #runtime_library_dirs=['/usr/lib/x86_64-linux-gnu/hdf5/serial'],
+                            libraries = ['hdf5', 'hdf5_hl'],
+                            extra_compile_args = ['-Wno-discarded-qualifiers'],
+                            extra_link_args = ['-L/usr/lib/x86_64-linux-gnu/hdf5/serial']
+                            )
 
-setup (name = 'pytrexio',
-       version = '0.1',
-       author      = "TREX-CoE",
-       description = """Python API of the TREXIO library""",
-       ext_modules = [pytrexio_module],
-       py_modules = ["pytrexio"],
-       url='https://github.com/TREX-CoE/trexio',
-       packages=['distutils', 'distutils.command'],
-       )
+setup(name             = 'pytrexio',
+      version          = '0.1',
+      author           = "TREX-CoE",
+      description      = """Python API of the TREXIO library""",
+      ext_modules      = [pytrexio_module],
+      py_modules       = ["pytrexio"],
+      url              = 'https://github.com/TREX-CoE/trexio',
+      license          = 'BSD',
+      packages         = ['distutils', 'distutils.command'],
+      install_requires = ['h5py']
+      )
