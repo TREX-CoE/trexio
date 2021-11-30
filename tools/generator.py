d3ba8f36525798e02e8aea029bf8e92ea579848d
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2021-11-30 16:30:36 +0100

    [WIP] working generator for sparse functions
    + remove trailing whitespaces

--- tools/generator.py
+++ tools/generator.py
@@ -6,18 +6,13 @@ config_file = 'trex.json'
 trex_config = read_json(config_file)
 # --------------------------------------------------------------------------- #
 
-# -------------------------------- [WIP] ------------------------------------ #
-# for now remove rdm from config because it functions are hardcoded
-del trex_config['rdm']
-# --------------------------------------------------------------------------- #
-
 # -------------------- GET ATTRIBUTES FROM THE CONFIGURATION ---------------- #
 group_dict = get_group_dict(trex_config)
 detailed_nums = get_detailed_num_dict(trex_config)
 detailed_strs = get_detailed_str_dict(trex_config)
 # helper dictionaries that contain names of groups, nums or dsets as keys
 dsets = get_dset_dict(trex_config)
-detailed_dsets_nostr, detailed_dsets_str = split_dset_dict_detailed(dsets)
+detailed_dsets_nostr, detailed_dsets_str, detailed_dsets_sparse = split_dset_dict_detailed(dsets)
 detailed_dsets = detailed_dsets_nostr.copy()
 detailed_dsets.update(detailed_dsets_str)
 # consistency check for dimensioning variables
@@ -26,9 +21,9 @@ check_dim_consistency(detailed_nums, dsets)
 
 # -------------------- GET TEMPLATED FILES TO BE POPULATED ------------------ #
 source = ['front', 'text', 'hdf5']
-# build helper dictionaries with paths per source directory 
+# build helper dictionaries with paths per source directory
 template_paths = get_template_paths(source)
-# build helper dictionaries with source files per source directory 
+# build helper dictionaries with source files per source directory
 source_files = get_source_files(template_paths)
 # build helper dictionaries with templated files
 files_todo = get_files_todo(source_files)
@@ -48,13 +43,17 @@ for fname in files_todo['attr_num']:
 for fname in files_todo['attr_str']:
     recursive_populate_file(fname, template_paths, detailed_strs)
 
-# populate has/read/write_dset (numerical) functions with recursive scheme 
+# populate has/read/write_dset (numerical) functions with recursive scheme
 for fname in files_todo['dset_data']:
     recursive_populate_file(fname, template_paths, detailed_dsets_nostr)
 
-# populate has/read/write_dset (strings) functions with recursive scheme 
+# populate has/read/write_dset (strings) functions with recursive scheme
 for fname in files_todo['dset_str']:
     recursive_populate_file(fname, template_paths, detailed_dsets_str)
+
+# populate has/read/write_dset (sparse) functions with recursive scheme
+for fname in files_todo['dset_sparse']:
+    recursive_populate_file(fname, template_paths, detailed_dsets_sparse)
 
 # populate group-related functions with mixed (iterative+recursive) scheme [text backend]
 for fname in files_todo['group']:
--- tools/generator_tools.py
+++ tools/generator_tools.py
@@ -4,7 +4,7 @@ from json import load as json_load
 
 
 def read_json(fname: str) -> dict:
-    """ 
+    """
     Read configuration from the input `fname` JSON file.
 
             Parameters:
@@ -23,7 +23,7 @@ def read_json(fname: str) -> dict:
 
 
 def get_files_todo(source_files: dict) -> dict:
-    """ 
+    """
     Build dictionaries of templated files per objective.
 
             Parameters:
@@ -36,21 +36,21 @@ def get_files_todo(source_files: dict) -> dict:
     for key in source_files.keys():
         all_files += source_files[key]
 
-    files_todo = {}       
+    files_todo = {}
     #files_todo['all'] = list(filter(lambda x: 'read' in x or 'write' in x or 'has' in x or 'hrw' in x or 'flush' in x or 'free' in x, all_files))
     files_todo['all'] = [f for f in all_files if 'read' in f or 'write' in f or 'has' in f or 'flush' in f or 'free' in f or 'hrw' in f]
-    for key in ['dset_data', 'dset_str', 'attr_num', 'attr_str', 'group']:
+    for key in ['dset_data', 'dset_str', 'dset_sparse', 'attr_num', 'attr_str', 'group']:
         files_todo[key] = list(filter(lambda x: key in x, files_todo['all']))
 
     files_todo['group'].append('struct_text_group_dset.h')
     # files that correspond to iterative population (e.g. the code is repeated within the function body but the function itself is unique)
-    files_todo['auxiliary'] = ['def_hdf5.c', 'basic_hdf5.c', 'basic_text_group.c', 'struct_hdf5.h', 'struct_text_group.h'] 
+    files_todo['auxiliary'] = ['def_hdf5.c', 'basic_hdf5.c', 'basic_text_group.c', 'struct_hdf5.h', 'struct_text_group.h']
 
     return files_todo
 
 
 def get_source_files(paths: dict) -> dict:
-    """ 
+    """
     Build dictionaries of all files per source directory.
 
             Parameters:
@@ -67,7 +67,7 @@ def get_source_files(paths: dict) -> dict:
 
 
 def get_template_paths(source: list) -> dict:
-    """ 
+    """
     Build dictionary of the absolute paths to directory with templates per source.
 
             Parameters:
@@ -86,7 +86,7 @@ def get_template_paths(source: list) -> dict:
 
 
 def recursive_populate_file(fname: str, paths: dict, detailed_source: dict) -> None:
-    """ 
+    """
     Populate files containing basic read/write/has functions.
 
             Parameters:
@@ -107,6 +107,8 @@ def recursive_populate_file(fname: str, paths: dict, detailed_source: dict) -> N
                 'group_num_f_dtype_default', 'group_num_f_dtype_double', 'group_num_f_dtype_single',
                 'group_num_dtype_default', 'group_num_dtype_double', 'group_num_dtype_single',
                 'group_num_h5_dtype', 'group_num_py_dtype',
+                'group_sparse_dset', 'group_sparse_dset_format_scanf', 'group_sparse_dset_format_printf',
+                'group_sparse_dset_line_length', 'group_sparse_dset_indices_printf', 'group_sparse_dset_indices_scanf',
                 'group_dset', 'group_num', 'group_str', 'group']
 
     for item in detailed_source.keys():
@@ -133,9 +135,9 @@ def recursive_populate_file(fname: str, paths: dict, detailed_source: dict) -> N
                     elif 'uncommented by the generator for dimensioning' in line:
                         # only uncomment and write the line if `num` is in the name
                         if 'dim' in detailed_source[item]['trex_json_int_type']:
-                            templine = line.replace('//', '') 
+                            templine = line.replace('//', '')
                             f_out.write(templine)
-                    # general case of recursive replacement of inline triggers 
+                    # general case of recursive replacement of inline triggers
                     else:
                         populated_line = recursive_replace_line(line, triggers, detailed_source[item])
                         f_out.write(populated_line)
@@ -144,8 +146,8 @@ def recursive_populate_file(fname: str, paths: dict, detailed_source: dict) -> N
 
 
 def recursive_replace_line (input_line: str, triggers: list, source: dict) -> str:
-    """ 
-    Recursive replacer. Recursively calls itself as long as there is at least one "$" present in the `input_line`. 
+    """
+    Recursive replacer. Recursively calls itself as long as there is at least one "$" present in the `input_line`.
 
             Parameters:
                     input_line (str)    : input line
@@ -154,10 +156,10 @@ def recursive_replace_line (input_line: str, triggers: list, source: dict) -> st
 
             Returns:
                     output_line (str)   : processed (replaced) line
-    """    
+    """
     is_triggered = False
     output_line = input_line
-    
+
     if '$' in input_line:
         for case in triggers:
             test_case = f'${case}$'
@@ -175,12 +177,12 @@ def recursive_replace_line (input_line: str, triggers: list, source: dict) -> st
         else:
             print(output_line)
             raise ValueError('Recursion went wrong, not all cases considered')
-        
+
     return output_line
 
 
 def iterative_populate_file (filename: str, paths: dict, groups: dict, datasets: dict, numbers: dict, strings: dict) -> None:
-    """ 
+    """
     Iteratively populate files with unique functions that contain templated variables.
 
             Parameters:
@@ -200,7 +202,7 @@ def iterative_populate_file (filename: str, paths: dict, groups: dict, datasets:
     templ_path = get_template_path(filename, paths)
     filename_out = join('populated',f'pop_{filename}')
 # Note: it is important that special conditions like add_trigger above will be checked before standard triggers
-# that contain only basic $-ed variable (like $group$). Otherwise, the standard triggers will be removed 
+# that contain only basic $-ed variable (like $group$). Otherwise, the standard triggers will be removed
 # from the template and the special condition will never be met.
     with open(join(templ_path,filename), 'r') as f_in :
         with open(join(templ_path,filename_out), 'a') as f_out :
@@ -225,13 +227,13 @@ def iterative_populate_file (filename: str, paths: dict, groups: dict, datasets:
                     f_out.write(populated_line)
                 else:
                     f_out.write(line)
-                    
+
             f_out.write("\n")
 
 
 def iterative_replace_line (input_line: str, case: str, source: dict, add_line: str) -> str:
-    """ 
-    Iterative replacer. Iteratively copy-pastes `input_line` each time with a new substitution of a templated variable depending on the `case`. 
+    """
+    Iterative replacer. Iteratively copy-pastes `input_line` each time with a new substitution of a templated variable depending on the `case`.
 
             Parameters:
                     input_line (str)    : input line
@@ -241,7 +243,7 @@ def iterative_replace_line (input_line: str, case: str, source: dict, add_line: 
 
             Returns:
                     output_block (str)   : processed (replaced) block of text
-    """    
+    """
     output_block = ""
     for item in source.keys():
         templine1 = input_line.replace(case.upper(), item.upper())
@@ -270,12 +272,12 @@ def check_triggers (input_line: str, triggers: list) -> int:
         if trig in input_line or trig.upper() in input_line:
             out_id = id
             return out_id
-        
+
     return out_id
 
 
 def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detailed_dset: dict, detailed_numbers: dict, detailed_strings: dict) -> None:
-    """ 
+    """
     Special population for group-related functions in the TEXT back end.
 
             Parameters:
@@ -316,11 +318,11 @@ def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detai
                     elif 'START REPEAT GROUP_NUM' in line or 'START REPEAT GROUP_ATTR_STR' in line:
                         subloop_num = True
                         continue
-                    
+
                     if 'END REPEAT GROUP_DSET' in line:
 
                         for dset in detailed_dset.keys():
-                            if group != detailed_dset[dset]['group']: 
+                            if group != detailed_dset[dset]['group']:
                                 continue
 
                             if ('REPEAT GROUP_DSET_STR' in line) and (detailed_dset[dset]['dtype'] != 'char*'):
@@ -351,7 +353,7 @@ def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detai
 
                     elif 'END REPEAT GROUP_NUM' in line:
                         for dim in detailed_numbers.keys():
-                            if group != detailed_numbers[dim]['group']: 
+                            if group != detailed_numbers[dim]['group']:
                                 continue
 
                             save_body = loop_body
@@ -364,7 +366,7 @@ def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detai
 
                     elif 'END REPEAT GROUP_ATTR_STR' in line:
                         for str in detailed_strings.keys():
-                            if group != detailed_strings[str]['group']: 
+                            if group != detailed_strings[str]['group']:
                                 continue
 
                             str_allocated.append(str)
@@ -390,22 +392,22 @@ def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detai
                         continue
 
                     if not subloop_num and not subloop_dset:
-                        # NORMAL CASE WITHOUT SUBLOOPS 
+                        # NORMAL CASE WITHOUT SUBLOOPS
                         if '$group_dset' in line:
                             for dset in detailed_dset.keys():
-                                if group != detailed_dset[dset]['group']: 
+                                if group != detailed_dset[dset]['group']:
                                     continue
                                 populated_line = recursive_replace_line(line, triggers, detailed_dset[dset])
                                 f_out.write(populated_line)
                         elif '$group_str' in line:
                             for str in detailed_strings.keys():
-                                if group != detailed_strings[str]['group']: 
+                                if group != detailed_strings[str]['group']:
                                     continue
                                 populated_line = recursive_replace_line(line, triggers, detailed_strings[str])
                                 f_out.write(populated_line)
                         elif '$group_num$' in line:
                             for dim in detailed_numbers.keys():
-                                if group != detailed_numbers[dim]['group']: 
+                                if group != detailed_numbers[dim]['group']:
                                     continue
                                 populated_line = recursive_replace_line(line, triggers, detailed_numbers[dim])
                                 f_out.write(populated_line)
@@ -421,7 +423,7 @@ def special_populate_text_group(fname: str, paths: dict, group_dict: dict, detai
 
 
 def get_template_path (filename: str, path_dict: dict) -> str:
-    """ 
+    """
     Returns the absolute path to the directory with indicated `filename` template.
 
             Parameters:
@@ -435,12 +437,12 @@ def get_template_path (filename: str, path_dict: dict) -> str:
         if dir_type in filename:
             path = path_dict[dir_type]
             return path
-        
+
     raise ValueError('Filename should contain one of the keywords')
 
 
 def get_group_dict (configuration: dict) -> dict:
-    """ 
+    """
     Returns the dictionary of all groups.
 
             Parameters:
@@ -457,9 +459,9 @@ def get_group_dict (configuration: dict) -> dict:
 
 
 def get_detailed_num_dict (configuration: dict) -> dict:
-    """ 
+    """
     Returns the dictionary of all `num`-suffixed variables.
-    Keys are names, values are subdictionaries containing corresponding group and group_num names. 
+    Keys are names, values are subdictionaries containing corresponding group and group_num names.
 
             Parameters:
                     configuration (dict) : configuration from `trex.json`
@@ -503,7 +505,7 @@ def get_detailed_num_dict (configuration: dict) -> dict:
                         tmp_dict['group_num_dtype_single'] = 'int32_t'
                         tmp_dict['default_prec']   = '32'
                         tmp_dict['group_num_std_dtype_out'] = '" PRId64 "'
-                        tmp_dict['group_num_std_dtype_in']  = '" SCNd64 "' 
+                        tmp_dict['group_num_std_dtype_in']  = '" SCNd64 "'
                         tmp_dict['group_num_py_dtype'] = 'int'
                         tmp_dict['trex_json_int_type'] = v2[0]
 
@@ -536,8 +538,8 @@ def get_detailed_str_dict (configuration: dict) -> dict:
 
 
 def get_dset_dict (configuration: dict) -> dict:
-    """ 
-    Returns the dictionary of datasets. 
+    """
+    Returns the dictionary of datasets.
     Keys are names, values are lists containing datatype, list of dimensions and group name
 
             Parameters:
@@ -559,8 +561,8 @@ def get_dset_dict (configuration: dict) -> dict:
 
 
 def split_dset_dict_detailed (datasets: dict) -> tuple:
-    """ 
-    Returns the detailed dictionary of datasets. 
+    """
+    Returns the detailed dictionary of datasets.
     Keys are names, values are subdictionaries containing substitutes for templated variables
 
             Parameters:
@@ -571,9 +573,11 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
     """
     dset_numeric_dict = {}
     dset_string_dict = {}
+    dset_sparse_dict = {}
     for k,v in datasets.items():
         # create a temp dictionary
         tmp_dict = {}
+        is_sparse = False
         # specify details required to replace templated variables later
         if v[0] == 'float':
             datatype = 'double'
@@ -599,7 +603,7 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
             group_dset_dtype_single = 'int32_t'
             default_prec   = '32'
             group_dset_std_dtype_out = '" PRId64 "'
-            group_dset_std_dtype_in  = '" SCNd64 "' 
+            group_dset_std_dtype_in  = '" SCNd64 "'
             group_dset_py_dtype = 'int'
         elif v[0] == 'str':
             datatype = 'char*'
@@ -612,11 +616,28 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
             group_dset_dtype_single = ''
             default_prec   = ''
             group_dset_std_dtype_out = 's'
-            group_dset_std_dtype_in  = 's' 
+            group_dset_std_dtype_in  = 's'
             group_dset_py_dtype = 'str'
-        
+        elif 'sparse' in v[0]:
+            is_sparse = True
+            datatype = 'double'
+            group_dset_h5_dtype       = 'native_double'
+            group_dset_f_dtype_default= 'real(8)'
+            group_dset_f_dtype_double = 'real(8)'
+            group_dset_f_dtype_single = 'real(4)'
+            group_dset_dtype_default= 'double'
+            group_dset_dtype_double = 'double'
+            group_dset_dtype_single = 'float'
+            default_prec   = '64'
+            group_dset_std_dtype_out = '24.16e'
+            group_dset_std_dtype_in = 'lf'
+            group_dset_py_dtype = 'float'
+
         # add the dset name for templates
-        tmp_dict['group_dset'] = k
+        if is_sparse:
+            tmp_dict['group_sparse_dset'] = k
+        else:
+            tmp_dict['group_dset'] = k
         # add flag to detect index types
         if 'index' == v[0]:
             tmp_dict['is_index'] = 'file->one_based'
@@ -625,7 +646,7 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
         # add the datatypes for templates
         tmp_dict['dtype'] = datatype
         tmp_dict['group_dset_dtype'] = datatype
-        tmp_dict['group_dset_h5_dtype'] = group_dset_h5_dtype 
+        tmp_dict['group_dset_h5_dtype'] = group_dset_h5_dtype
         tmp_dict['group_dset_f_dtype_default'] = group_dset_f_dtype_default
         tmp_dict['group_dset_f_dtype_double'] = group_dset_f_dtype_double
         tmp_dict['group_dset_f_dtype_single'] = group_dset_f_dtype_single
@@ -647,7 +668,7 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
             for i in range(1, tmp_dict['rank']):
                 dim_toadd = tmp_dict['dims'][i]
                 dim_list += f', {dim_toadd}'
-        
+
         tmp_dict['group_dset_dim_list'] = dim_list
 
         if tmp_dict['rank'] == 0:
@@ -656,21 +677,30 @@ def split_dset_dict_detailed (datasets: dict) -> tuple:
             dim_f_list = "(*)"
         tmp_dict['group_dset_f_dims'] = dim_f_list
 
+        if is_sparse:
+            tmp_dict['group_sparse_dset_format_scanf'] = "%d %d %d %d %lf"
+            tmp_dict['group_sparse_dset_format_printf'] = "%10d %10d %10d %10d %24.16e"
+            tmp_dict['group_sparse_dset_line_length'] = "69"
+            tmp_dict['group_sparse_dset_indices_printf'] = "index_sparse[4*i], index_sparse[4*i+1], index_sparse[4*i+2], index_sparse[4*i+3]"
+            tmp_dict['group_sparse_dset_indices_scanf']  = "&index_sparse[4*i], &index_sparse[4*i+1], &index_sparse[4*i+2], &index_sparse[4*i+3]"
+
         # add group name as a key-value pair to the dset dict
         tmp_dict['group'] = v[2]
 
         # split datasets in numeric- and string- based
         if (datatype == 'char*'):
             dset_string_dict[k] = tmp_dict
+        elif (is_sparse):
+            dset_sparse_dict[k] = tmp_dict
         else:
             dset_numeric_dict[k] = tmp_dict
 
-    return (dset_numeric_dict, dset_string_dict)
+    return (dset_numeric_dict, dset_string_dict, dset_sparse_dict)
 
 
 def check_dim_consistency(num: dict, dset: dict) -> None:
-    """ 
-    Consistency check to make sure that each dimensioning variable exists as a num attribute of some group. 
+    """
+    Consistency check to make sure that each dimensioning variable exists as a num attribute of some group.
 
             Parameters:
                     num (dict)  : dictionary of numerical attributes
