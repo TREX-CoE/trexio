62a2b2bb749e784b56a3143c078f583ba677a266
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2022-10-04 15:42:23 +0200

    Fix pre_close test

--- tests/pre_close.c
+++ tests/pre_close.c
@@ -5,8 +5,8 @@
 #include <stdint.h>
 
 #define TEST_BACKEND  TREXIO_TEXT
-#define TREXIO_FILE   "test_dset_sparse.dir"
-#define RM_COMMAND    "rm -rf " TREXIO_FILE
+#define TREXIO_FILE 	"test_pre_close.dir"
+#define RM_COMMAND 	"rm -f -- " TREXIO_FILE "/*.txt " TREXIO_FILE "/*.txt.size " TREXIO_FILE "/.lock && rm -fd -- " TREXIO_FILE
 
 static int test_pre_close_1 (const char* file_name, const back_end_t backend)
 {
