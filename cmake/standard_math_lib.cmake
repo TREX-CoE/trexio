14bc5888727a30a71db778ef6bca8a5facc83007
Robert Adam <dev@robert-adam.de>
2025-08-19 16:32:02 +0200

    Link against standard math library as necessary
    Fixes #225

---
+++ cmake/standard_math_lib.cmake
@@ -0,0 +1,43 @@
+# This function checks if the C standard library includes the symbols for
+# math operations and if not, will try to locate the auxiliary library (libm on Unix)
+# and link the given target to it.
+function(target_link_std_math TARGET VISIBILITY)
+  include(CheckCSourceCompiles)
+
+  # Clear variable to not be affected by external code
+  set(CMAKE_REQUIRED_LIBRARIES "")
+
+  set(TEST_SOURCE
+"
+#include <math.h>
+
+int main(int argc, char **argv) {
+return (int) (log(*argv[0]) + sin(*argv[1]) + sqrt(*argv[2]));
+}
+"
+  )
+
+  set(VAR_BASENAME "${PROJECT_NAME}_COMPILES_C")
+
+  check_c_source_compiles("${TEST_SOURCE}" "${VAR_BASENAME}")
+
+  if ("${VAR_BASENAME}")
+    # No explicit linking to math library required
+    return()
+  endif()
+
+  find_library(STD_MATH_LIB "m")
+
+  if (NOT STD_MATH_LIB)
+    message(FATAL_ERROR "Linking to std math library required but the library could not be found")
+  endif()
+  
+  set(CMAKE_REQUIRED_LIBRARIES "${STD_MATH_LIB}")
+  check_c_source_compiles("${TEST_SOURCE}" "${VAR_BASENAME}_WITH_STDMATH")
+
+  if (NOT "${${VAR_BASENAME}_WITH_STDMATH}")
+    message(FATAL_ERROR "Program doesn't compile even though linked against '${STD_MATH_LIB}'")
+  endif()
+  
+  target_link_libraries("${TARGET}" "${VISIBILITY}" "${STD_MATH_LIB}")
+endfunction()
